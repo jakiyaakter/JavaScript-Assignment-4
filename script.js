5f529075234dd03f4055c39517431f9293824a81
jakiyaakter <jakkiyaakter744@gmai.com>
2024-02-20 14:53:42 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var applePrice = 60;
+var chocolatePrice =30;
+var myBudget = 20;
+
+
+if (applePrice<myBudget == chocolatePricePrice){
+    console.log("apple available");
+}
+else{
+    console.log('chocolate khabo');
+}
